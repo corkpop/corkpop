2e32f44a93683f530197f5f639a9d578176b4141
corkpop <d.iakymuk@gmail.com>
2016-10-13 17:00:18 +0200

    Add files via upload

---
+++ Friday.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20131127
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('Friday_files/NewTravel_C_TL.png'),IWCreateImage('Friday_files/NewTravel_S_T.png'),IWCreateImage('Friday_files/NewTravel_C_TR.png'),IWCreateImage('Friday_files/NewTravel_S_R.png'),IWCreateImage('Friday_files/NewTravel_C_BR.png'),IWCreateImage('Friday_files/NewTravel_S_B.png'),IWCreateImage('Friday_files/NewTravel_C_BL.png'),IWCreateImage('Friday_files/NewTravel_S_L.png')],null,1,0.573684,71.000000,0.000000,62.000000,62.000000,83.000000,9.000000,73.000000,77.000000,8.000000,8.000000,8.000000,9.000000,null,null,null,0.400000),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(0.1736,0.9848),color:'#817b67',opacity:0.700000}),stroke_0:new IWPhotoFrame([IWCreateImage('Friday_files/receipt_top_01.png'),IWCreateImage('Friday_files/receipt_top_02.png'),IWCreateImage('Friday_files/receipt_top_03.png'),IWCreateImage('Friday_files/receipt_side_02.png'),IWCreateImage('Friday_files/receipt_bottom_03.png'),IWCreateImage('Friday_files/receipt_bottom_02.png'),IWCreateImage('Friday_files/receipt_bottom_01.png'),IWCreateImage('Friday_files/receipt_side_01.png')],null,2,1.000000,0.000000,2.000000,0.000000,8.000000,1.000000,9.000000,1.000000,41.000000,1.000000,275.000000,1.000000,275.000000,null,null,null,0.500000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Friday_files/FridayMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Friday_files','shapeimage_4','0');applyEffects()}
