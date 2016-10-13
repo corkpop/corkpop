a3b6d5b77c51c9b1025abc39d611d4fc867001b8
corkpop <d.iakymuk@gmail.com>
2016-10-13 17:05:37 +0200

    Add files via upload

---
+++ images/Tloen.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20131127
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(0.1736,0.9848),color:'#817b67',opacity:0.700000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.750000}),stroke_0:new IWPhotoFrame([IWCreateImage('Tloen_files/NewTravel_C_TL.png'),IWCreateImage('Tloen_files/NewTravel_S_T.png'),IWCreateImage('Tloen_files/NewTravel_C_TR.png'),IWCreateImage('Tloen_files/NewTravel_S_R.png'),IWCreateImage('Tloen_files/NewTravel_C_BR.png'),IWCreateImage('Tloen_files/NewTravel_S_B.png'),IWCreateImage('Tloen_files/NewTravel_C_BL.png'),IWCreateImage('Tloen_files/NewTravel_S_L.png')],null,1,0.670000,71.000000,0.000000,62.000000,62.000000,83.000000,9.000000,73.000000,77.000000,8.000000,8.000000,8.000000,9.000000,null,null,null,0.400000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Tloen_files/TloenMoz.css')
+fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');fixupIECSS3Opacity('id2');IMpreload('Tloen_files','shapeimage_7','0');applyEffects()}
